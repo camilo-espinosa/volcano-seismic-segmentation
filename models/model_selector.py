214c49f7ba11777d7435c591659e4f712a11bfff
Camilo <camilo.e94@gmail.com>
2025-05-06 06:53:01 -0400

    test1

--- models/model_selector.py
+++ models/model_selector.py
@@ -4,7 +4,9 @@ from SwinUNet import SwinTransformerSys
 from UNet import UNet
 from PhaseNet import PhaseNet
 import requests
+
 import os
+
 
 def model_selector(arch, N=256, pretrained=True):
     device = "cuda" if cuda.is_available() == True else "cpu"
@@ -12,8 +14,8 @@ def model_selector(arch, N=256, pretrained=True):
         model = UNet2(in_channels=1, out_channels=6, init_features=16, depth=5)
     elif arch == "DeepLabV3":
         model = smp.DeepLabV3Plus(
-            encoder_depth=5, 
-            decoder_channels=128,  
+            encoder_depth=5,
+            decoder_channels=128,
             encoder_name="mobilenet_v2",
             in_channels=1,
             classes=6,
@@ -22,13 +24,13 @@ def model_selector(arch, N=256, pretrained=True):
         )
     elif arch == "UNetPlusPlus":
         model = smp.UnetPlusPlus(
-            encoder_name="timm-efficientnet-b1", 
+            encoder_name="timm-efficientnet-b1",
             in_channels=1,
             classes=6,
             encoder_weights=None,
             activation="softmax2d",
             decoder_channels=[64, 32, 16],
-            encoder_depth=3, 
+            encoder_depth=3,
         )
     elif arch == "SwinUNet":
         model = SwinTransformerSys(
@@ -36,10 +38,10 @@ def model_selector(arch, N=256, pretrained=True):
             patch_size=4,
             in_chans=1,
             num_classes=6,
-            embed_dim=48,  
+            embed_dim=48,
             depths=[2, 2, 2, 2],
             depths_decoder=[1, 2, 2, 2],
-            num_heads=[3, 6, 8, 12],  
+            num_heads=[3, 6, 8, 12],
             window_size=8,
         )
     elif arch == "PhaseNet":
@@ -53,35 +55,43 @@ def model_selector(arch, N=256, pretrained=True):
             filters_root=32,
         )
     if pretrained:
-        doi = '10.5281/zenodo.13902232'
-        record_id = doi.split('.')[-1]  
+        doi = "10.5281/zenodo.13902232"
+        record_id = doi.split(".")[-1]
         metadata_url = f"https://zenodo.org/api/records/{record_id}"
         response = requests.get(metadata_url)
         metadata = response.json()
-        files = metadata['files']
+        files = metadata["files"]
         if arch == "UNet":
-            file_to_download = files[4] # 1: UNet.pt - https://zenodo.org/api/records/13902232/files/UNet.pt/content
+            file_to_download = files[
+                4
+            ]  # 1: UNet.pt - https://zenodo.org/api/records/13902232/files/UNet.pt/content
         elif arch == "DeepLabV3":
-            file_to_download = files[1] # 2: DeepLabV3Plus.pt - https://zenodo.org/api/records/13902232/files/DeepLabV3Plus.pt/content
+            file_to_download = files[
+                1
+            ]  # 2: DeepLabV3Plus.pt - https://zenodo.org/api/records/13902232/files/DeepLabV3Plus.pt/content
         elif arch == "UNetPlusPlus":
-            file_to_download = files[3] # 3: UNetPlusPlus.pt - https://zenodo.org/api/records/13902232/files/UNetPlusPlus.pt/content
+            file_to_download = files[
+                3
+            ]  # 3: UNetPlusPlus.pt - https://zenodo.org/api/records/13902232/files/UNetPlusPlus.pt/content
         elif arch == "SwinUNet":
-            file_to_download = files[0] # 4: SwinUNet.pt - https://zenodo.org/api/records/13902232/files/SwinUNet.pt/content
+            file_to_download = files[
+                0
+            ]  # 4: SwinUNet.pt - https://zenodo.org/api/records/13902232/files/SwinUNet.pt/content
         elif arch == "PhaseNet":
-            file_to_download = files[2] # 5: SwinUNet.pt - https://zenodo.org/api/records/13902232/files/SwinUNet.pt/content
-        file_url = file_to_download['links']['self']
-        weights_path = file_to_download['key']
+            file_to_download = files[
+                2
+            ]  # 5: SwinUNet.pt - https://zenodo.org/api/records/13902232/files/SwinUNet.pt/content
+        file_url = file_to_download["links"]["self"]
+        weights_path = file_to_download["key"]
         if os.path.exists(weights_path):
-            print(f"{weights_path} Already downloaded")   
+            print(f"{weights_path} Already downloaded")
         else:
             file_response = requests.get(file_url)
-            with open(weights_path, 'wb') as f:
+            with open(weights_path, "wb") as f:
                 f.write(file_response.content)
-            print(f"Downloaded {weights_path}")   
+            print(f"Downloaded {weights_path}")
         print(f"Loading weights...")
-        model.load_state_dict(load(weights_path, map_location=device)["model_state_dict"])
+        model.load_state_dict(
+            load(weights_path, map_location=device)["model_state_dict"]
+        )
     return model
-
-
-
-
